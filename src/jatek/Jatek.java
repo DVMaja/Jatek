10a9c307c333f4f61d100f006cd61385950f803e
DVMaja <majadreilinger@gmail.com>
2023-12-11 11:41:04 +0100

    mentés folyamatban error van egyenlőre

--- src/jatek/Jatek.java
+++ src/jatek/Jatek.java
@@ -1,12 +1,13 @@
 package jatek;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Jatek {
-
-    private void mentes() {
-    }
-
-    private void belvasas() {
-    }
 
     public void run() {
         Karakter k1 = new Karakter();
@@ -24,5 +25,26 @@ public class Jatek {
         k1.hasznal("kard", 2, true);
         System.out.println("k1: " + k1.toString());
         //Mentsd el a jelenlegi játék állást
+        mentes(k1);
+        //k1 = null;
+        //System.out.println("k1: " + k1.toString());
+    }
+
+    private void mentes(Karakter k1) {
+        System.out.println("Mentés");
+        try {
+            ObjectOutputStream objKi = new ObjectOutputStream((new FileOutputStream(("karakter_k1.bin"))));
+            objKi.writeObject(k1);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Jatek.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Jatek.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("mentve");
+
+    }
+
+    private void belvasas() {
+
     }
 }

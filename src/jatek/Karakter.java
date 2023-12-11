3d05598f46a74e05a3e63992a603cf762713f43c
DVMaja <majadreilinger@gmail.com>
2023-12-11 09:20:38 +0100

    eletero, szerencse, ugyesseg random gen megvan

--- src/jatek/Karakter.java
+++ src/jatek/Karakter.java
@@ -1,11 +1,20 @@
 package jatek;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Karakter {
 
     private int eletero, ugyesseg, szerencse;
     private ArrayList<Targy> felszereles; //String lista
+    Random rnd;
+
+    public Karakter() {
+        //r.nextInt(high-low) + low;
+        this.eletero = ((rnd.nextInt(1, 7) + 6) * 2) + 12;
+        this.ugyesseg = (rnd.nextInt(1, 7) + 6) + 6;
+        this.szerencse = (rnd.nextInt(1, 7) + 6) + 6;
+    }
 
     public void felvesz(Targy targy) {
         felszereles.add(targy);
